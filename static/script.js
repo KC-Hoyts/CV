a8a478d7a82797b6f99188c50142c23995d2db14
KC-Hoyts <ga45k@mail.ru>
2024-01-30 00:23:19 +0800

    Added container .outside for better padding for inside skills blocks (button and p)

--- static/script.js
+++ static/script.js
@@ -14,13 +14,13 @@ for (i = 0; i < coll.length; i++) {
     
     let content = this.nextElementSibling;
     if (content.style.maxHeight){
-        content.setAttribute("style", "padding-bottom: 0px; width: 95%")
+        content.setAttribute("style", "padding-bottom: 0px;")
         content.style.maxHeight = null;
         this.setAttribute("style", "border-radius: 10px 10px 10px 10px");
     
     } else {
         this.setAttribute("style", "border-radius: 10px 10px 0px 0px");
-        content.setAttribute("style", "border-radius: 0px 0px 25px 25px; padding-bottom: 15px");
+        content.setAttribute("style", "border-radius: 0px 0px 25px 25px; padding-bottom: 15px;");
         content.style.maxHeight = content.scrollHeight + "px";
         
     //   сверачивание всех остальных пунктов
@@ -28,7 +28,7 @@ for (i = 0; i < coll.length; i++) {
             if (coll[j].innerHTML != this.innerHTML) {
                 content_for_close = coll[j].nextElementSibling;
                 if (content_for_close.style.maxHeight != null) {
-                    content_for_close.setAttribute("style", "padding-bottom: 0px; width: 95%")
+                    content_for_close.setAttribute("style", "padding-bottom: 0px;")
                     content_for_close.style.maxHeight = null;
                     coll[j].classList.remove("active"); 
                     coll[j].setAttribute("style", "border-radius: 10px 10px 10px 10px");
